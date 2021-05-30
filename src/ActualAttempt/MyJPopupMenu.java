8660b69cc621c2091eb35dc7019dfd0446f2aedb
JFraire1 <Jose.Fraire@student.csulb.edu>
2021-05-29 23:32:35 -0700

    copy and paste, basic functionality

--- src/ActualAttempt/MyJPopupMenu.java
+++ src/ActualAttempt/MyJPopupMenu.java
@@ -2,17 +2,23 @@ package ActualAttempt;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeNode;
-import javax.swing.tree.TreePath;
+import javax.swing.tree.DefaultTreeModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class MyJPopupMenu extends JPopupMenu {
     private final JTree tree;
     private final JPanel parentPanel;
     private final DefaultMutableTreeNode selectedNode;
-    private final String dir;
+    private static String sourceDir;
+    private static boolean hasSourceDir = false;
+    private String dir;
 
     public MyJPopupMenu(DefaultMutableTreeNode selectedNode, String dir, JPanel parentPanel, JTree tree){
         this.selectedNode = selectedNode;
@@ -23,10 +29,10 @@ public class MyJPopupMenu extends JPopupMenu {
         JMenuItem delete = new JMenuItem("Delete");
         JMenuItem paste = new JMenuItem("Paste");
         JMenuItem rename = new JMenuItem("Rename");
-        copy.addActionListener(new MyMenuActionListener());
-        delete.addActionListener(new MyMenuActionListener());
-        paste.addActionListener(new MyMenuActionListener());
-        rename.addActionListener(new MyMenuActionListener());
+        copy.addActionListener(new CopyActionListener());
+        //delete.addActionListener(new ActionListener());
+        paste.addActionListener(new PasteActionListener());
+        //rename.addActionListener(new MyMenuActionListener());
         add(copy);
         add(paste);
         add(rename);
@@ -34,28 +40,43 @@ public class MyJPopupMenu extends JPopupMenu {
     }
 
 
-    private class MyMenuActionListener implements ActionListener{
+    private class CopyActionListener implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
-            switch(e.getActionCommand())
-            {
-                case "Copy":
-                    //to DO
-                    System.out.println("Not implemented yet");
-                    System.out.println(dir);
-                case "Delete":
-                    //to Do
-                    ;
-                case "Paste":
-                    //to Do
-                    ;
-                case "Rename":
-                    //to Do
-                    ;
-            }
+            sourceDir = dir;
+            hasSourceDir = true;
         }
     }
 
-
+    private class PasteActionListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(!hasSourceDir){return;}
+            File file = new File(dir);
+            File sourceFile = new File(sourceDir);
+            if (!sourceFile.exists()){
+                hasSourceDir = false;
+                return;
+            }
+            if (!file.isDirectory()){
+                String[] dirList = dir.split("\\\\");
+                dir = "";
+                for (int i=0; i<dirList.length-1;i++){
+                    dir += dirList[i] + "\\";
+                }
+                dir += sourceFile.getName();
+            }
+            else{
+                dir += "\\" + sourceFile.getName();
+            }
+            //implement whole directory copying and pasting
+            try {
+                Files.copy(Paths.get(sourceDir), Paths.get(dir), REPLACE_EXISTING);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            //implement refreshing of JTree, likely need to make new interface implemented by both DirPanel and FilePanel with method buildTree, and then call Treemodel.reload().
+        }
+    }
 
 }
